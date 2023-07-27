7bec7d0366752bd26ad700ea588b64d973f2ea14
Reyna May <roma.reynamayA@gmail.com>
2023-07-27 15:55:17 -0600

    Update the functions to use the lighthousebnb database with SQL queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -25,14 +25,21 @@ pool.query(`SELECT title FROM properties LIMIT 10;`)
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query(`SELECT * FROM users
+    WHERE email = $1;`, [email])
+    .then((res) => {
+      // Check if any rows were returned from the query
+      if (res.rows.length === 0) {
+        // user not found, resolve with null
+        return null;
+      }
+      console.log(res.rows[0]);
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -41,7 +48,21 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users
+    WHERE id = $1`, [id])
+    .then((res) => {
+      // Check if any rows were returned from the query
+      if (res.rows.length === 0) {
+        // user not found, resolve with null
+        return null;
+      }
+      console.log(res.rows[0]);
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -50,10 +71,18 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, password, email)
+    VALUES($1, $2, $3) RETURNING *;`, [user.name, user.password, user.email])
+    .then((res) => {
+      const newUser = res.rows[0];
+      console.log('New user added:', newUser);
+      return newUser;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      throw err;
+    });
 };
 
 /// Reservations
