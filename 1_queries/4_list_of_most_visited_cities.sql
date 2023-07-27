f214e2cca56cfb5f39238c6973950b1016e2f521
Reyna May <roma.reynamayA@gmail.com>
2023-07-26 19:35:42 -0600

    Add query to ge the list of most visited cities

---
+++ 1_queries/4_list_of_most_visited_cities.sql
@@ -0,0 +1,6 @@
+SELECT properties.city AS city,
+  COUNT(reservations) AS total_reservations
+FROM reservations
+JOIN properties ON properties.id = property_id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
