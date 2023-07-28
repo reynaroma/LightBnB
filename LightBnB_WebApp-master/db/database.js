041c73b8fd839833e5f8190d16a315eba63bc7e1
Reyna May <roma.reynamayA@gmail.com>
2023-07-27 21:19:40 -0600

    Update the function of addProperties to the properties table

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -9,7 +9,7 @@ const config = {
   password: 123,
   host: 'localhost',
   database: 'lightbnb'
-}
+};
 
 const pool = new Pool(config);
 
@@ -206,10 +206,25 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `INSERT INTO properties
+    (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+    parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`;
+
+  const values = [property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
+    property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms,
+    property.country, property.street, property.city, property.province, property.post_code];
+
+  return pool
+    .query(queryString, values)
+    .then((res) => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
 };
 
 module.exports = {
